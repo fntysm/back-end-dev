f4052ba5241233a4e44214dfa516da8454cc9c03
fntysm <zainebb7k9h1@gmail.com>
2023-01-04 17:50:42 +0100

    ---

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-require('dotenv').config;
+//require('dotenv').config;
 // to declare our application variable by importing express (which is itself a function)
 const app = require("express")();
-
+const PORT=8080;
 // to use external middlewares
-app.use(express.json())
+//app.use(express.json())
 // define the router ()
 app.get('/',(req,res)=>{res.json({message:'welcome to GDGs workshop'})})
 //fire the api by nmaking it listen to the PORT
-app.listen(process.env.PORT||8080, ()=>console.log(`it's running on the local host: http://localhost:${PORT}`))
+app.listen(PORT, ()=>console.log(`it's running on the local host: http://localhost:${PORT}`))
