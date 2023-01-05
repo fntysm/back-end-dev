174cfa3af7a1759aab119de0512b1b6c05a1b821
fntysm <zainebb7k9h1@gmail.com>
2023-01-05 14:02:21 +0100

    -

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const PORT=8080;
 // to use external middlewares
 //app.use(express.json())
 // define the router ()
-app.get('/',(req,res)=>{res.json({message:'welcome to GDGs workshop'})})
+app.get('/',(req,res)=>{res.status(200).json({situation:'works',message:'gdg && wtm'})})
 //fire the api by nmaking it listen to the PORT
 app.listen(PORT, ()=>console.log(`it's running on the local host: http://localhost:${PORT}`))
